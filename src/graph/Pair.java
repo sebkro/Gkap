2923c8ce20314cc671d5641e11294b3e8402c7b1
Sebastian Krome <Sebastian@Wall-E.(none)>
2011-12-01 12:29:42 +0100

    pair

--- src/graph/Pair.java
+++ src/graph/Pair.java
@@ -1,6 +1,27 @@
 package graph;
 
-public interface Pair {
-	public <X> X getFirst();
-	public <Y> Y getSecond();
+public class Pair<X, Y> {
+	
+	X first;
+	Y second;
+	
+	public Pair(){
+		
+	}
+	
+	public X getFirst(){
+		return first;
+	}
+	
+	public Y getSecond(){
+		return second;
+	}
+	
+	public void setFirst(X first){
+		this.first = first;
+	}
+	
+	public void setSecond(Y second){
+		this.second = second;
+	}
 }
